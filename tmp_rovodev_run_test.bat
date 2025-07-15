0a0c19ec3601b7cdc0bc49f120d61d5c8babd137
abra5umente <87623193+abra5umente@users.noreply.github.com>
2025-07-16 02:50:59 +1000

    Delete tmp_rovodev_run_test.bat

--- tmp_rovodev_run_test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Running direct test...
-"C:\Program Files\Go\bin\go.exe" run tmp_rovodev_direct_test.go
-echo.
-echo Exit code: %ERRORLEVEL%
-echo.
-echo Press any key to continue...
-pause > nul
